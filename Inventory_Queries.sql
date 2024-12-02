d3ed8f5d1bb7b0c8606f5cbc1eac6329de36b188
Grikee <152575528+Grikee@users.noreply.github.com>
2024-12-01 20:17:00 -0800

    Update Inventory_Queries.sql
    Added 3 queries. Stock level monitoring, top 10, revenue and sales volume by product category

--- Inventory_Queries.sql
+++ Inventory_Queries.sql
@@ -14,3 +14,35 @@ FROM `bigquery-public-data.thelook_ecommerce.products` AS p
 JOIN `bigquery-public-data.thelook_ecommerce.inventory_items` AS inv ON p.id = inv.product_id
 GROUP BY p.category
 ORDER BY turnover_rate DESC
+
+--3. Stock Level Monitoring
+SELECT ii.product_id, ii.product_name, COUNT(ii.id) AS stock_quantity
+FROM `bigquery-public-data.thelook_ecommerce.inventory_items` AS ii
+WHERE ii.sold_at IS NULL -- Unsold items indicate current stock
+GROUP BY ii.product_id, ii.product_name
+HAVING COUNT(ii.id) < 5 -- Threshold for low stock
+ORDER BY stock_quantity ASC;
+
+
+--4. Top 10 best selling products
+SELECT oi.product_id, p.name AS product_name, SUM(oi.sale_price) AS total_sales_revenue, COUNT(oi.id) AS total_quantity_sold
+FROM `bigquery-public-data.thelook_ecommerce.order_items` AS oi
+JOIN `bigquery-public-data.thelook_ecommerce.products` AS p
+ON oi.product_id = p.id
+GROUP BY oi.product_id, p.name
+ORDER BY total_quantity_sold DESC
+LIMIT 10;
+
+
+--5. Revenue and sales volume by product category
+SELECT p.category AS product_category, SUM(oi.sale_price) AS total_revenue, COUNT(oi.id) AS total_units_sold
+FROM `bigquery-public-data.thelook_ecommerce.order_items` AS oi
+JOIN `bigquery-public-data.thelook_ecommerce.products` AS p
+ON oi.product_id = p.id
+GROUP BY p.category
+ORDER BY total_revenue DESC
+LIMIT 10;
+
+
+
+
