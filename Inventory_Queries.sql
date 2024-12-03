2d54c9a0443c187e1996f44289cd7f3e12852fcf
rsmithiiee <137942077+rsmithiiee@users.noreply.github.com>
2024-12-02 17:51:44 -0800

    Update Inventory_Queries.sql
    Edited top selling products query to top selling products by user selected category

--- Inventory_Queries.sql
+++ Inventory_Queries.sql
@@ -57,3 +57,19 @@ JOIN `bigquery-public-data.thelook_ecommerce.products` AS p ON oi.id=p.id
 GROUP BY p.name
 ORDER BY total_orders ASC
 LIMIT 10;
+
+--FINAL QUERIES
+
+--Show top 10 selling products by user selected category
+--Declare variable for category selection
+--Can use 'Accessories' as another category in demo
+DECLARE select_category STRING;
+SET select_category = 'Shorts';
+
+SELECT oi.product_id, p.name AS product_name, SUM(oi.sale_price) AS total_sales_revenue, COUNT(oi.id) AS total_quantity_sold
+FROM `bigquery-public-data.thelook_ecommerce.order_items` AS oi
+JOIN `bigquery-public-data.thelook_ecommerce.products` AS p ON oi.product_id = p.id
+WHERE p.category = select_category
+GROUP BY oi.product_id, p.name
+ORDER BY total_quantity_sold DESC, total_sales_revenue DESC
+LIMIT 10;
